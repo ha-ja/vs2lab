8a9ed9284bb2dd4c1e04bf18ebf2be1104332773
walterfit <fiwa1011@h-ka.de>
2024-04-24 09:14:47 +0200

    Loesung fuer lab2 -Walter

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,4 +1,6 @@
 import constRPC
+import time
+import threading
 
 from context import lab_channel
 
@@ -16,19 +18,44 @@ class Client:
     def __init__(self):
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
+        self.channel = lab_channel.Channel()
+        self.clientMiddleware = self.channel.join('clientmiddleware')
         self.server = None
+        self.middleware = None
 
     def run(self):
         self.chan.bind(self.client)
         self.server = self.chan.subgroup('server')
+        self.channel.bind(self.clientMiddleware)
+        self.middleware = self.channel.subgroup('middleware')
 
     def stop(self):
         self.chan.leave('client')
+        self.channel.leave('middleware')
 
-    def append(self, data, db_list):
+    @staticmethod
+    def recieve_ack(self, data, db_list):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
-        self.chan.send_to(self.server, msglst)  # send msg to server
+        # Funktion, die im Thread ausgeführt wird
+        def thread_function():
+            self.chan.send_to(self.server, msglst)  # send msg to server
+
+        # Erstellen und Starten des Threads
+        thread1 = threading.Thread(target=thread_function)
+
+        self.channel.send_to(self.middleware, msglst) # send msg to middleware
+        ack = self.channel.receive_from(self.middleware)  # wait for response
+        print("ACK: ", ack)
+        thread1.start()
+        
+        # Warten, bis der Thread beendet ist
+        thread1.join()
+        
+            
+    
+    def append(self, data, db_list):
+        self.recieve_ack(self, data, db_list)
         msgrcv = self.chan.receive_from(self.server)  # wait for response
         return msgrcv[1]  # pass it to caller
 
@@ -37,6 +64,8 @@ class Server:
     def __init__(self):
         self.chan = lab_channel.Channel()
         self.server = self.chan.join('server')
+        self.channel = lab_channel.Channel()
+        self.middleware = self.channel.join('middleware')
         self.timeout = 3
 
     @staticmethod
@@ -46,11 +75,19 @@ class Server:
 
     def run(self):
         self.chan.bind(self.server)
+        self.channel.bind(self.middleware)
         while True:
+            msgreqmiddleware = self.channel.receive_from_any(self.timeout)  # wait for any request
+            if msgreqmiddleware is not None:
+                client = msgreqmiddleware[0]  # see who is the caller
+                self.channel.send_to({client}, '200 OK')
+
             msgreq = self.chan.receive_from_any(self.timeout)  # wait for any request
             if msgreq is not None:
                 client = msgreq[0]  # see who is the caller
+                self.channel.send_to({client}, '200 OK')
                 msgrpc = msgreq[1]  # fetch call & parameters
+                time.sleep(7)
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
                     result = self.append(msgrpc[1], msgrpc[2])  # do local call
                     self.chan.send_to({client}, result)  # return response
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,5 +1,6 @@
 import rpc
 import logging
+import threading
 
 from context import lab_logging
 
@@ -8,9 +9,27 @@ lab_logging.setup(stream_level=logging.INFO)
 cl = rpc.Client()
 cl.run()
 
-base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+# Funktion, die im Thread ausgeführt wird
+def thread_function(name, callback):
+    base_list = rpc.DBList({'foo'})
+    result_list = cl.append('bar', base_list)
+    callback(result_list)
 
-print("Result: {}".format(result_list.value))
+# Callback-Funktion, die aufgerufen wird, wenn der Thread beendet ist
+def callback_function(result_list):
+    print("Result: {}".format(result_list.value))
+
+# Erstellen und Starten des Threads
+thread1 = threading.Thread(target=thread_function, args=("Thread 1", callback_function))
+thread1.start()
+
+print("Wo bin ich?")
+print("Was bin ich?")
+print("HAALLLOOOOO?")
+
+
+# Warten, bis der Thread beendet ist
+thread1.join()
+
 
 cl.stop()
