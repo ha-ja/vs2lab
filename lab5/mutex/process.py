90aa69c1fd3c930c673d29fe4ce35cc02ecd4bb6
walterfit <fiwa1011@h-ka.de>
2024-06-16 12:57:18 +0200

    Aufgabe 5 erledigt

--- lab5/mutex/process.py
+++ lab5/mutex/process.py
@@ -74,7 +74,7 @@ class Process:
     # Erhöcht die eigene clock und sendet Nachricht mit Prozess der entfernt werden soll
     def __remove_failed_process(self, failed_process):
         self.clock = self.clock + 1  # Increment clock value
-        msg = (self.clock, failed_process, REMOVE)
+        msg = (self.clock, failed_process, REMOVE) # Sende Nachricht mit der id des Ausgefallenen Prozess 
         self.channel.send_to(self.other_processes, msg)  # Permit other
 
     def __release(self):
@@ -135,7 +135,7 @@ class Process:
             self.__cleanup_queue()  # Finally sort and cleanup the queue
         else:        
             self.logger.warning("{} timed out on RECEIVE.".format(self.__mapid()))
-            if self.timeout_count == 0 and self.queue[0][2] == '1' and self.queue[0][1] == self.process_id:
+            if self.timeout_count < len(self.queue) and self.timeout_count == 0 and self.queue[0][2] == '1' and self.queue[0][1] == self.process_id:
                 working_processes = [entry[1] for entry in self.queue if entry[2] == '2']
                 working_processes.append(self.process_id)
                 failed_process = ""
@@ -156,6 +156,7 @@ class Process:
                 failed_process = self.queue[0][1]
                 self.logger.warning("Detected failure of process: {}".format(self.__mapid(failed_process)))
                 self.__remove_failed_process(failed_process)
+                self.clock = self.clock + 1  # Increment clock value
                 # Entfernt den ausgefallen Prozess aus der Liste der anderen Prozesse
                 if failed_process in self.other_processes:
                     self.other_processes.remove(failed_process)
@@ -164,7 +165,7 @@ class Process:
                 # Loggt das der ausgefallene Prozess entfernt wurde.
                 self.logger.info("Removed failed process: {}".format(self.__mapid(failed_process)))
                 self.__cleanup_queue()  # Finally sort and cleanup the queue
-                self.timeout_count = 0
+                self.timeout_count = 0 # Zähler zurücksetzen
             self.timeout_count += 1
 
     def init(self):
